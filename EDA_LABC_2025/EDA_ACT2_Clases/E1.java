6e42ecf49ae171062ad4ace77aebc9be7215f422
jtijero <jtijero@unsa.edu.pe>
2025-05-20 16:34:31 -0500

    Implementar un método recursivo que imprima en forma descendente de 5 a 1 de uno en uno

--- EDA_LABC_2025/EDA_ACT2_Clases/E1.java
+++ EDA_LABC_2025/EDA_ACT2_Clases/E1.java
@@ -2,13 +2,13 @@ package EDA_ACT2_Clases;
 
 public class E1 { //recursividad
   void imprimir(int x){
-    if(x==0){
+    if(x<10 && x>0){
       System.out.println(x);
       imprimir(x-1);
     }
   }
   
-  public static void main(String[] arg ){
+  public static void main(String[] args){
     E1 recursividad = new E1();
     recursividad.imprimir(5);
   }
